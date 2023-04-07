c5e5fd1b50c7af9f3033f793c4ba10ace271e248
yoo16 <yohei.yoshikawa@gmail.com>
2023-04-07 23:56:21 +0900

    update

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,20 +1,13 @@
 package com.example.demo.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("title", "Top");
+    public String index() {
         return "home/index";
     }
 
