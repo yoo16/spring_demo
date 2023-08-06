c456d616adc1cdbc3f2599af108ae85c5b458b49
yoo16 <yohei.yoshikawa@gmail.com>
2023-08-07 08:57:46 +0900

    update

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -4,7 +4,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
