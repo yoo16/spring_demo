732150330263ed2e496a2f2bc36c0ff45c0616f2
yoo16 <yohei.yoshikawa@gmail.com>
2023-08-07 09:41:12 +0900

    update

--- src/main/java/com/example/demo/controllers/RestSampleController.java
+++ src/main/java/com/example/demo/controllers/RestSampleController.java
@@ -2,13 +2,11 @@ package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class RestSampleController {
-    @GetMapping("/hello")
-    public String index(@RequestParam String name) {
-        String message = "Hello, " + name;
-        return message;
-    }
+   @GetMapping("/hello")
+public String index(@RequestParam(name = "name", required = false) String name) {
+     String message = "Hello, " + name;
+     return message;
+} 
 }
