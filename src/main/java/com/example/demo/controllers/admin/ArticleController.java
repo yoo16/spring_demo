d8de0eeae7384bc40e35034e94a45b3d1fb92310
yoo16 <yohei.yoshikawa@gmail.com>
2023-09-11 12:53:07 +0900

    update

--- src/main/java/com/example/demo/controllers/admin/ArticleController.java
+++ src/main/java/com/example/demo/controllers/admin/ArticleController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controllers.admin;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
